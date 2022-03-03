3aacd0ad7194024642589396db579148643901bd
ppambler <allinlbj@gmail.com>
2022-03-03 18:18:57 +0800

    获取有关 Video 详情页的三个数据

--- service/api_video.js
+++ service/api_video.js
@@ -1,8 +1,41 @@
 import hyRequest from "./index";
 
+/**
+ * 请求 MV 的排行
+ * @param {number} offset 
+ * @param {number} [limit=10] - 默认值为 10
+ */
 export function getTopMV(offset, limit = 10) {
   return hyRequest.get("/top/mv", {
     offset,
     limit,
   });
+}
+/**
+ * 请求 MV 的播放地址
+ * @param {number} id - MV 的 id
+ */
+export function getMVURL(id) {
+  return hyRequest.get("/mv/url", {
+    id
+  })
+}
+
+/**
+ * 请求 MV 的详情
+ * @param {number} mvid - MV 的 id
+ */
+export function getMVDetail(mvid) {
+  return hyRequest.get("/mv/detail", {
+    mvid
+  })
+}
+/**
+ * 请求相关视频
+ * @param {number} id - MV 的 id
+ */
+export function getRelatedVideo(id) {
+  return hyRequest.get("/related/allvideo", {
+    id
+  })
 }
