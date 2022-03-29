34ebf969e818e599cc055a17648ee586ed52fd59
ppambler <allinlbj@gmail.com>
2022-03-29 19:09:26 +0800

    歌曲详情页-榜单数据展示

--- pages/detail-songs/index.js
+++ pages/detail-songs/index.js
@@ -1,17 +1,22 @@
 // pages/detail-songs/index.js
+import { rankingStore } from '../../store/index'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    songInfo: {}
   },
   onLoad: function (options) {
     const ranking = options.ranking
-    console.log(ranking)
+    // 1.获取数据
+    rankingStore.onState(ranking, this.getRankingDataHanlder)
   },
   onUnload: function () {
-
+    rankingStore.offState(this.data.ranking, this.getRankingDataHanlder)
   },
+  getRankingDataHanlder: function(res) {
+    this.setData({ songInfo: res })
+  } 
 })
