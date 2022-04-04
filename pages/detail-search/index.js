1710f346f46a8fdfba3e9351d1487716568add23
ppambler <allinlbj@gmail.com>
2022-04-04 20:21:23 +0800

    搜索页-高亮搜索建议中匹配到的字符

--- pages/detail-search/index.js
+++ pages/detail-search/index.js
@@ -9,7 +9,8 @@ Page({
   data: {
     hotKeywords: [],
     searchValue: "",
-    suggestSongs: []
+    suggestSongs: [],
+    suggestSongsNodes: []
   },
   onLoad: function (options) {
     // 1.获取页面的数据
@@ -39,7 +40,42 @@ Page({
 
     // 4.根据关键字进行搜索
     debounceGetSearchSuggest(searchValue).then(res => {
-      this.setData({ suggestSongs: res.result.allMatch })
+      // 1.获取建议的关键字歌曲
+      const suggestSongs = res.result.allMatch
+      this.setData({ suggestSongs })
+
+       // 2.转成 nodes 节点
+      const suggestKeywords = suggestSongs.map(item => item.keyword)
+      const suggestSongsNodes = []
+      for (const keyword of suggestKeywords) {
+        const nodes = []
+        if (keyword.toUpperCase().startsWith(searchValue.toUpperCase())) {
+          const key1 = keyword.slice(0, searchValue.length)
+          const node1 = {
+            name: "span",
+            attrs: { style: "color: #26ce8a;" },
+            children: [ { type: "text", text: key1 } ]
+          }
+          nodes.push(node1)
+      
+          const key2 = keyword.slice(searchValue.length)
+          const node2 = {
+            name: "span",
+            attrs: { style: "color: #000000;" },
+            children: [ { type: "text", text: key2 } ]
+          }
+          nodes.push(node2)
+        } else {
+          const node = {
+            name: "span",
+            attrs: { style: "color: #000000;" },
+            children: [ { type: "text", text: keyword } ]
+          } 
+          nodes.push(node)
+        }
+        suggestSongsNodes.push(nodes)
+      }
+      this.setData({ suggestSongsNodes })
     })
   }
 })
