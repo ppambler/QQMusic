e088f7f2728e5b98ebdd0070efbe6d009a938b56
ppambler <allinlbj@gmail.com>
2022-04-15 13:48:16 +0800

    播放页-获取播放歌曲的详情

--- pages/music-player/index.js
+++ pages/music-player/index.js
@@ -1,66 +1,28 @@
 // pages/music-player/index.js
-Page({
+import { getSongDetail } from '../../service/api_player'
 
+Page({
   /**
    * 页面的初始数据
    */
   data: {
-
+    id: 0,
+    currentSong: {}
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
+    // 1.获取传入的id
+    const id = options.id
+    this.setData({ id })
 
+    // 2.根据id获取歌曲信息
+    this.getPageData(id)
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  // 网络请求
+  getPageData: function (id) {
+    getSongDetail(id).then(res => {
+      this.setData({ currentSong: res.songs[0] })
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
   onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
   }
 })
---
+++ service/api_player.js
@@ -0,0 +1,7 @@
+import hyRequest from './index'
+
+export function getSongDetail(ids) {
+  return hyRequest.get("/song/detail", {
+    ids
+  })
+}
