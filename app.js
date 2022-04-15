5960d8fb50cac6ba2218c10eb48e7c2556b5e03b
ppambler <allinlbj@gmail.com>
2022-04-15 19:05:15 +0800

    播放页-封装自定义可复用的导航栏

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 // app.js
 App({
-  
+  onLaunch: function () {
+    const info = wx.getSystemInfoSync()
+    this.globalData.statusBarHeight = info.statusBarHeight
+  },
+  globalData: {
+    statusBarHeight: 0,
+    navBarHeight: 44
+  }
 })
