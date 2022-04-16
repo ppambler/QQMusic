cc21800d4eee34a364ce502fe271562ebcd1d440
ppambler <allinlbj@gmail.com>
2022-04-16 19:49:14 +0800

    播放页-播放页的分页布局展示

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ App({
   onLaunch: function () {
     const info = wx.getSystemInfoSync()
     this.globalData.statusBarHeight = info.statusBarHeight
+    this.globalData.screenWidth = info.screenWidth
+    this.globalData.screenHeight = info.screenHeight
   },
   globalData: {
     statusBarHeight: 0,
-    navBarHeight: 44
+    navBarHeight: 44,
+    screenWidth: 0,
+    screenHeight: 0
   }
 })
--- pages/music-player/index.js
+++ pages/music-player/index.js
@@ -8,7 +8,8 @@ Page({
   data: {
     id: 0,
     currentSong: {},
-    currentPage: 0
+    currentPage: 0,
+    contentHeight: 0
   },
   onLoad: function (options) {
     // 1.获取传入的id
@@ -17,12 +18,25 @@ Page({
 
     // 2.根据id获取歌曲信息
     this.getPageData(id)
+
+     // 3.动态计算内容高度
+     const globalData = getApp().globalData
+     const screenHeight = globalData.screenHeight
+     const statusBarHeight = globalData.statusBarHeight
+     const navBarHeight = globalData.navBarHeight
+     const contentHeight = screenHeight - statusBarHeight - navBarHeight
+     this.setData({ contentHeight })
   },
   // 网络请求
   getPageData: function (id) {
     getSongDetail(id).then(res => {
       this.setData({ currentSong: res.songs[0] })
     })
+  },
+  // 事件处理
+  handleSwiperChange: function(event) {
+    const current = event.detail.current
+    this.setData({ currentPage: current })
   },
   onUnload: function () {
   }
