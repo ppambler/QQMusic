769f910c5d5921150a304b52aada76ded891a502
ppambler <allinlbj@gmail.com>
2022-05-16 21:51:18 +0800

    登录代码封装-登录状态-登录判断

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 // app.js
+
+import { getLoginCode, codeToToken, checkToken, checkSession } from './service/api_login'
+import { TOKEN_KEY } from './constants/token-const'
+
 App({
-  onLaunch: function () {
+  globalData: {
+    statusBarHeight: 0,
+    navBarHeight: 44,
+    screenWidth: 0,
+    screenHeight: 0,
+    deviceRadio: 0
+  },
+  onLaunch: async function () {
     const info = wx.getSystemInfoSync()
     this.globalData.statusBarHeight = info.statusBarHeight
     this.globalData.screenWidth = info.screenWidth
@@ -9,12 +20,27 @@ App({
     const deviceRadio = info.screenHeight / info.screenWidth
     // console.log(deviceRadio)
     this.globalData.deviceRadio = deviceRadio
+
+    // 2.让用户默认进行登录
+    const token = wx.getStorageSync(TOKEN_KEY)
+    // token 有没有过期
+    const checkResult = await checkToken(token)
+    console.log(checkResult)
+    // 判断 session 是否过期
+    const isSessionExpire = await checkSession()
+
+    // 没有 token 或 token 过期了 或 session 过期了
+    if (!token || checkResult.errorCode || !isSessionExpire) {
+      this.loginAction()
+    }
   },
-  globalData: {
-    statusBarHeight: 0,
-    navBarHeight: 44,
-    screenWidth: 0,
-    screenHeight: 0,
-    deviceRadio: 0
+  loginAction: async function() {
+    // 1.获取code
+    const code = await getLoginCode()
+
+    // 2.将code发送给服务器
+    const result = await codeToToken(code)
+    const token = result.token
+    wx.setStorageSync(TOKEN_KEY, token)
   }
 })
--- service/index.js
+++ service/index.js
@@ -1,11 +1,20 @@
 const BASE_URL = "http://123.207.32.32:9001";
 
+// 线上部署的
+const LOGIN_BASE_URL = "http://123.207.32.32:3000"
+// 自己部署的登录服务器代码
+// const LOGIN_BASE_URL = "http://localhost:3000"
+
 class HYRequest {
-  request(url, method, params) {
+  constructor(baseURL) {
+    this.baseURL = baseURL
+  }
+  request(url, method, params, header = {}) {
     return new Promise((resolve, reject) => {
       wx.request({
-        url: BASE_URL + url,
+        url: this.baseURL + url,
         method: method,
+        header: header,
         data: params,
         success: function (res) {
           resolve(res.data);
@@ -15,15 +24,20 @@ class HYRequest {
     });
   }
 
-  get(url, params) {
-    return this.request(url, "GET", params);
+  get(url, params, header) {
+    return this.request(url, "GET", params, header);
   }
 
-  post(url, data) {
-    return this.request(url, "POST", data);
+  post(url, data, header) {
+    return this.request(url, "POST", data, header);
   }
 }
 
-const hyRequest = new HYRequest();
+const hyRequest = new HYRequest(BASE_URL);
+const hyLoginRequest = new HYRequest(LOGIN_BASE_URL)
+
+export {
+  hyLoginRequest
+}
 
 export default hyRequest;
